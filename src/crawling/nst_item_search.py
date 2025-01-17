11da4d7d275f1793fb6bb553c1f0de6e3b0feb8e
SangGyeom <vhf1030@naver.com>
2025-01-17 20:57:27 +0900

    Add trend data

--- src/crawling/nst_item_search.py
+++ src/crawling/nst_item_search.py
@@ -7,6 +7,7 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import platform
+import pandas as pd
 
 
 def select_dropdown_option(driver, button_id: str, option_xpath: str):
@@ -127,4 +128,116 @@ def extract_relative_trend_score(driver):
     trend_score_relative = trend_score[1] / trend_score[0]
     return trend_score_relative
 
+
+def run_fetch(keyword, base_keyword):
+    # 네이버 트렌드 데이터 수집 진행
+    # 품목 리스트
+    # item_name_list = "농어, 광어, 대게, 연어, 우럭, 참돔, 방어".split(', ')
+
+    # 1. URL 접속
+    driver = webdriver.Chrome()
+
+    url = "https://datalab.naver.com/"
+    driver.get(url)                 # URL로 이동
+
+    # 기본 옵션 설정
+
+    # # 범위 설정 (월 기준)
+    # select_dropdown_option(driver, "timeDimensionTitle", "//li[contains(@class, '_item_month')]/a")
+    # # 범위 설정 (주 기준)
+    # select_dropdown_option(driver, "timeDimensionTitle", "//li[contains(@class, '_item_week')]/a")
+    # 범위 설정 (일 기준)
+    select_dropdown_option(driver, "timeDimensionTitle", "//li[contains(@class, '_item_date')]/a")
+    time.sleep(1)
+
+    # 시작 연월 설정
+    select_dropdown_option(driver, "startYear", "//div[@id='startYearDiv']//li[contains(@class, '_item_2016')]/a")
+    select_dropdown_option(driver, "startMonth", "//div[@id='startMonthDiv']//li[contains(@class, '_item_01')]/a")
+    select_dropdown_option(driver, "startDay", "//div[@id='startDayDiv']//li[contains(@class, '_item_01')]/a")
+    time.sleep(1)
+
+    # 종료 연월 설정
+    select_dropdown_option(driver, "endYear", "//div[@id='endYearDiv']//li[contains(@class, '_item_2024')]/a")
+    select_dropdown_option(driver, "endMonth", "//div[@id='endMonthDiv']//li[contains(@class, '_item_12')]/a")
+    select_dropdown_option(driver, "endDay", "//div[@id='endDayDiv']//li[contains(@class, '_item_31')]/a")
+    time.sleep(1)
+
+    # 연령 체크박스 정보
+    age_info = {
+        '06_12': "item_age_1",
+        '13_18': "item_age_2",
+        '19_24': "item_age_3",
+        '25_29': "item_age_4",
+        '30_34': "item_age_5",
+        '35_39': "item_age_6",
+        '40_44': "item_age_7",
+        '45_49': "item_age_8",
+        '50_54': "item_age_9",
+        '55_59': "item_age_10",
+        '60_80': "item_age_11",
+    }
+
+    trend_data = []
+    # for item in item_name_list:
+    for age in age_info:
+        for uncheck in age_info.values():  # 체크박스 초기화
+            toggle_checkbox(driver, uncheck, check=False)
+        toggle_checkbox(driver, age_info[age], check=True)
+        # for gt in game_trend_list:
+        #     input_keywords(driver, gt)  # 키워드 입력
+
+        # if (age, item) in [(t['age'], t['name']) for t in trend_data]:
+        #     continue
+        input_keyword(driver, baseline_keyword=base_keyword, input_keyword=keyword+","+base_keyword)  # 키워드 입력
+        search_btn = driver.find_element(
+            By.CSS_SELECTOR,
+            "#content > div._search_trend_wrapper > div.keyword_trend > div > div > form > fieldset > a, #content > div.section_keyword > div.keyword_trend > div.section_step > div.com_box_inner > form > fieldset > a"
+        )
+        search_btn.click()  # 조회버튼 클릭
+        print(age, keyword, end="/")
+        # print(gt)
+        time.sleep(2)
+        # trend_score = extract_relative_trend_score(driver)
+        chart_class_list = ["bb-target-data" + str(i) for i in range(2)]
+        score_tmp = []
+        for chart_class in chart_class_list:
+            circles = driver.find_elements(By.CSS_SELECTOR, f"g.{chart_class} circle")
+            data_points = []
+            for circle in circles:
+                cy = circle.get_attribute("cy")  # cy 속성 추출
+                if cy:  # cy 값이 존재하는 경우만 추가
+                    # data_points.append(469 - float(cy))
+                    # for g, s in zip(gt, trend_score):
+                    data_points.append(469 - float(cy))
+            score_tmp.append(data_points)
+        for i, s in enumerate(zip(*score_tmp)):
+            trend_data.append({
+                "idx": i,
+                "age": age,
+                "name": keyword,
+                "score": s[1]/s[0]
+            })
+        print(len([t for t in trend_data if t["name"] == keyword and t["age"] == age]))
+    return trend_data
+
+
+def run_crawling():
+    item_name_list = "농어, 광어, 대게, 연어, 우럭, 참돔, 방어".split(', ')
+    today = time.strftime("%Y-%m-%d")
+
+    for item in item_name_list:
+        trend_data = run_fetch(item, "회")
+        df = pd.DataFrame(trend_data)
         
+        # 시작 날짜 설정
+        start_date = pd.Timestamp("2016-01-01")
+
+        # idx를 날짜로 변환
+        df['date'] = start_date + pd.to_timedelta(df['idx'], unit='D')
+
+        # 파일로 저장
+        output_file = f"data/raw/nst_{item}_trend_{today}.csv"
+        df.to_csv(output_file, index=False, encoding="utf-8")
+        
+        
+run_crawling()
